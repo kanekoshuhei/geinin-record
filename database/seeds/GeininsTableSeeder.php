a5647b1307cba0f03465eee161d30c63fa75f3be
Shuhei Kaneko <shukonda@gmail.com>
2018-12-05 16:45:51 +0900

    Seederファイル追加

--- database/seeds/GeininsTableSeeder.php
+++ database/seeds/GeininsTableSeeder.php
@@ -1,21 +1,219 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
-use App\Geinin;
 
 class GeininsTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        $geinin = new Geinin;
-        $geinin->name = '';
-        $geinin->office = '';
-        $geinin->save();
+        
+
+        \DB::table('geinins')->delete();
+        
+        \DB::table('geinins')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'ナイツ',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'CmTziW4tcng',
+                'office' => 'マセキ芸能社',
+                'formed_year' => 2000,
+                'created_at' => '2018-12-03 14:20:34',
+                'updated_at' => '2018-12-05 05:50:02',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'プラス・マイナス',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'f0vFi21K8BM',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 2003,
+                'created_at' => '2018-12-03 08:21:16',
+                'updated_at' => '2018-12-05 05:50:26',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => '和牛',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'yMuMdz7nGUM',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 2006,
+                'created_at' => '2018-12-03 08:23:27',
+                'updated_at' => '2018-12-05 05:50:54',
+            ),
+            3 => 
+            array (
+                'id' => 5,
+                'name' => 'ジャルジャル',
+                'member_1' => '後藤淳平',
+                'member_2' => '福徳秀介',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'b1J39WQFvSs',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 2003,
+                'created_at' => '2018-12-04 01:44:54',
+                'updated_at' => '2018-12-04 01:44:54',
+            ),
+            4 => 
+            array (
+                'id' => 6,
+                'name' => '東京03',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'o4v2W8I_wyo',
+                'office' => 'プロダクション人力舎',
+                'formed_year' => 2003,
+                'created_at' => '2018-12-04 01:57:48',
+                'updated_at' => '2018-12-05 05:47:28',
+            ),
+            5 => 
+            array (
+                'id' => 7,
+                'name' => 'サンドウィッチマン',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'KFvfNh8kbg4',
+                'office' => 'グレープカンパニー',
+                'formed_year' => 1998,
+                'created_at' => '2018-12-04 01:58:55',
+                'updated_at' => '2018-12-05 05:43:09',
+            ),
+            6 => 
+            array (
+                'id' => 8,
+                'name' => 'バイきんぐ',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'z3AFlqqxqJs',
+                'office' => 'SMA NEET Project',
+                'formed_year' => 1998,
+                'created_at' => '2018-12-04 02:05:04',
+                'updated_at' => '2018-12-05 05:52:56',
+            ),
+            7 => 
+            array (
+                'id' => 9,
+                'name' => 'ハリウッドザコシショウ',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'E22VJZFGed4',
+                'office' => 'SMA NEET Project',
+                'formed_year' => 1993,
+                'created_at' => '2018-12-04 02:06:06',
+                'updated_at' => '2018-12-05 05:54:44',
+            ),
+            8 => 
+            array (
+                'id' => 11,
+                'name' => 'キングコング',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => '3zupJ5BboJo',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 1999,
+                'created_at' => '2018-12-04 02:26:50',
+                'updated_at' => '2018-12-05 05:56:18',
+            ),
+            9 => 
+            array (
+                'id' => 12,
+                'name' => 'ラーメンズ',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'iqqX6de2CNY',
+                'office' => 'トゥインクル・コーポレーション',
+                'formed_year' => 1996,
+                'created_at' => '2018-12-04 02:29:39',
+                'updated_at' => '2018-12-05 05:56:52',
+            ),
+            10 => 
+            array (
+                'id' => 13,
+                'name' => 'バカリズム',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'AID47o4Jbtw',
+                'office' => 'マセキ芸能社',
+                'formed_year' => 1995,
+                'created_at' => '2018-12-04 02:30:30',
+                'updated_at' => '2018-12-05 05:57:31',
+            ),
+            11 => 
+            array (
+                'id' => 14,
+                'name' => 'ロバート',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'AslvLTReeQs',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 1998,
+                'created_at' => '2018-12-04 02:31:50',
+                'updated_at' => '2018-12-05 05:58:35',
+            ),
+            12 => 
+            array (
+                'id' => 16,
+                'name' => '陣内智則',
+                'member_1' => '',
+                'member_2' => '',
+                'member_3' => '',
+                'member_4' => '',
+                'member_5' => '',
+                'image' => 'HeQgMWn3D38',
+                'office' => 'よしもとクリエイティブ・エージェンシー',
+                'formed_year' => 1993,
+                'created_at' => '2018-12-04 02:38:24',
+                'updated_at' => '2018-12-04 02:38:24',
+            ),
+        ));
+        
+        
     }
 }
